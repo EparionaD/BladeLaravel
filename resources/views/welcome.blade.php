b191b30439fa7218fe8527690d2b930fa3350b30
Ernesto Pariona Diaz <eparionad@gmail.com>
2020-02-06 21:30:41 -0500

    plantilla

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,92 @@
+@extends('base/base')
+
+@section('seccion')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="jumbotron jumbotron-fluid my-3">
+                    <div class="container">
+                      <h1 class="display-4">Ernesto Pariona Díaz</h1>
+                      <p class="lead">Bienvenido a mi Portafolio, aqui encontrarás los trabajos que realice durante mis años de programador.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="card-columns">
+                <div class="card">
+                    <img src="{{ asset('storage/img/web.png') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Página web X</h5>
+                        <p class="card-text">Desarrollo de una web</p>
+                        <a href="#" class="btn btn-primary">Visitar</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('storage/img/web2.jpg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Página web X</h5>
+                        <p class="card-text">Desarrollo de una web</p>
+                        <a href="#" class="btn btn-primary">Visitar</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('storage/img/web3.png') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Página web X</h5>
+                        <p class="card-text">Desarrollo de una web</p>
+                        <a href="#" class="btn btn-primary">Visitar</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('storage/img/web4.jpg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Página web X</h5>
+                        <p class="card-text">Desarrollo de una web</p>
+                        <a href="#" class="btn btn-primary">Visitar</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('storage/img/web5.png') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Página web X</h5>
+                        <p class="card-text">Desarrollo de una web</p>
+                        <a href="#" class="btn btn-primary">Visitar</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('storage/img/web6.jpg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Página web X</h5>
+                        <p class="card-text">Desarrollo de una web</p>
+                        <a href="#" class="btn btn-primary">Visitar</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('storage/img/web7.jpg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Página web X</h5>
+                        <p class="card-text">Desarrollo de una web</p>
+                        <a href="#" class="btn btn-primary">Visitar</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('storage/img/web8.png') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Página web X</h5>
+                        <p class="card-text">Desarrollo de una web</p>
+                        <a href="#" class="btn btn-primary">Visitar</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('storage/img/web9.jpg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Página web X</h5>
+                        <p class="card-text">Desarrollo de una web</p>
+                        <a href="#" class="btn btn-primary">Visitar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
